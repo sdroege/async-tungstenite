93d4b35d4d4073b88385e62fbcdc98ae80c80fdb
Sebastian Dröge <sebastian@centricular.com>
2019-11-26 22:43:40 +0100

    Remove unused #[pin_project] attribute

--- src/lib.rs
+++ src/lib.rs
@@ -176,9 +176,7 @@ where
 /// through the respective `Stream` and `Sink`. Check more information about
 /// them in `futures-rs` crate documentation or have a look on the examples
 /// and unit tests for this crate.
-#[pin_project]
 pub struct WebSocketStream<S> {
-    #[pin]
     inner: WebSocket<AllowStd<S>>,
 }
 
